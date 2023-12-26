e3fe65f07388397864a495162984afac8deedbc3
sqlmerr <sqlmerr@ya.ru>
2023-12-26 13:53:11 +0300

    2 Players

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,38 @@
 pub mod game {
+    use rand::Rng;
+
     pub struct Game {
         pub name: String
     }
     impl Game {
-        pub fn run(&self) {
+        pub fn run(&mut self) {
+            let mut rng = rand::thread_rng();
             let mut map = [["[ ]"; 3]; 3];
             println!("{}", self.name);
 
             let mut err = "";
+            let mut player = String::new();
+
+            let random_num = rng.gen_range(1..=2);
+            println!("{}", random_num);
+            if random_num == 1 {
+                player = "крестик".to_string();
+            } else {
+                player = "нолик".to_string();
+            }
+
 
             loop {
                 print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+                if player == "крестик" {
+                    player = "нолик".to_string();
+                } else {
+                    player = "крестик".to_string();
+                }
+
+
+                println!("Сейчас ходит {}", player);
                 for row in 0..3 {
                     let mut line = String::new();
                     for col in 0..3 {
@@ -32,7 +54,11 @@ pub mod game {
                         let cell_index = (num - 1) % 3;
 
                         if map[row][cell_index] == "[ ]" {
-                            map[row][cell_index] = "[x]"
+                            if player == "крестик" {
+                                map[row][cell_index] = "[x]"
+                            } else {
+                                map[row][cell_index] = "[o]"
+                            }
                         } else {
                             err = "Эта ячейка уже занята";
                             continue
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod game;
 use game::game::Game;
 
 fn main() {
-    let game: Game = Game {
+    let mut game: Game = Game {
         name: "TicTacToe by sqlmerr".to_string(),
     };
     game.run();
